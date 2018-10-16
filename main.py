bca13a9929e8e8794093a0a03d63f8c31cac1b0e
Hoang Doan <hoangdoan@chidoanh.com>
2018-10-16 15:14:02 +0700

    init source-code

---
+++ main.py
@@ -0,0 +1,72 @@
+import sublime
+import sublime_plugin
+import base64
+import hashlib
+import binascii
+
+class Base64decodeCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		v = self.view
+		try:
+			sels = len(list(v.sel()))
+			for i in range(0, sels):
+				selected = v.substr(v.sel()[i])
+				b64_str = base64.b64decode(bytes(selected,'utf-8')).decode('utf-8')
+				v.replace(edit, v.sel()[i], b64_str)
+		except Exception as err:
+			sublime.error_message(str(err))
+
+class Base64encodeCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		v = self.view
+		sels = len(list(v.sel()))
+		for i in range(0, sels):
+			selected = v.substr(v.sel()[i])
+			b64_str = base64.b64encode(selected.encode('utf-8')).decode("utf-8")
+			v.replace(edit, v.sel()[i], b64_str)
+
+class HexdecodeCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		v = self.view
+		sels = len(list(v.sel()))
+		try:
+			for i in range(0, sels):
+				selected = v.substr(v.sel()[i])
+				selected = selected.strip(' ')
+				selected = selected.replace('\\x','')
+				hex_bytes =binascii.unhexlify(selected)
+				v.replace(edit, v.sel()[i], str(hex_bytes, 'ascii'))
+		except Exception as err:
+			sublime.error_message(str(err))
+
+class HexencodeCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		v = self.view
+		sels = len(list(v.sel()))
+		for i in range(0, sels):
+			selected = v.substr(v.sel()[i])
+			selected = selected.strip(' ')
+			hex_bytes = binascii.hexlify(bytes(selected,'utf-8'))
+			v.replace(edit, v.sel()[i], str(hex_bytes, 'ascii'))
+
+class Hashmd5Command(sublime_plugin.TextCommand):
+	def run(self, edit):
+		v = self.view
+		sels = len(list(v.sel()))
+		m = hashlib.md5()
+		for i in range(0, sels):
+			selected = v.substr(v.sel()[i])
+			m.update(selected.encode('utf-8'))
+			v.replace(edit, v.sel()[i], m.hexdigest())
+
+class Hashsha1Command(sublime_plugin.TextCommand):
+	def run(self, edit):
+		v = self.view
+		sels = len(list(v.sel()))
+		m = hashlib.sha1()
+		for i in range(0, sels):
+			selected = v.substr(v.sel()[i])
+			m.update(selected.encode('utf-8'))
+			v.replace(edit, v.sel()[i], m.hexdigest())
+
+
